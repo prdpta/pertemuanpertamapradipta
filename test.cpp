655cf21262aec4284f232c143ba7974aa275dfe0
prdpta <pradiptaputramei01@gmail.com>
2024-02-27 19:28:06 +0700

    test

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+    float nJejari, nLuas; 
+    // numeric nJejari, nLuas
+    cout << "Masukan Jejari= ";
+    // display "Masukkan jejari"
+    cin >> nJejari;
+    // accept nJejari
+    nLuas = 3.14 * nJejari * nJejari;
+    cout << "Luasnya= " <<nLuas<<endl;
+
+    //test
+    
+
+
+
+
+    
+}
